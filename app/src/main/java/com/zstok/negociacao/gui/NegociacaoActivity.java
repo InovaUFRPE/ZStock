af93ae53b038cec5274f6f542eb238f2bb972c73
Lucas Marsol <Lucasmarsolv@hotmail.com>
2018-07-02 16:25:35 -0300

    Iniciando negociação

---
+++ app/src/main/java/com/zstok/negociacao/gui/NegociacaoActivity.java
@@ -0,0 +1,38 @@
+package com.zstok.negociacao.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.zstok.R;
+import com.zstok.infraestrutura.utils.VerificaConexao;
+
+public class NegociacaoActivity extends AppCompatActivity {
+
+    private TextView tvNegociacaoNomeEmpresa;
+    private EditText edtNegociacaoBarraMensagem;
+    private ListView lvMensagens;
+
+    private VerificaConexao verificaConexao;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_negociacao);
+
+        //Recuperando Views
+
+        Button btnNegociacaoCarrinho = findViewById(R.id.btnNegociacaoCarrinho);
+        Button btnNegociacaoOferta = findViewById(R.id.btnNegociacaoOferta);
+        Button btnNegociacaoEnviarMensagem = findViewById(R.id.btnNegociacaoEnviarMensagem);
+        tvNegociacaoNomeEmpresa = findViewById(R.id.tvNegociacaoNomeEmpresa);
+        edtNegociacaoBarraMensagem = findViewById(R.id.edtNegociacaoBarraMensagem);
+        lvMensagens = findViewById(R.id.lvMensagens);
+        
+
+    }
+}
